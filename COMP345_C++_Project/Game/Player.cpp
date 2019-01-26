67f1b114c17d5edc656f40b13b9f397ca43ab873
dvellucci <vellucci.dan@gmail.com>
2019-01-26 08:41:28 -0500

    Added skeleton code for map loading.

--- COMP345_C++_Project/Game/Player.cpp
+++ COMP345_C++_Project/Game/Player.cpp
@@ -9,7 +9,48 @@ Player::~Player()
 {
 }
 
+void Player::updatePlayerTokens(std::vector<std::shared_ptr<Map::City>> &m_ownedCities)
+{
+	for (std::shared_ptr<Map::City> region : m_ownedCities)
+	{
+		//region->m_tokenSprite->setTexture(ResourceHolder::Instance()->get(Textures::Title));
+		//region->m_tokenSprite->setPosition(region->m_xPos, region->m_yPos);
+	}
+}
+
 void Player::doPlayerTurn()
 {
 	return;
+}
+
+void Player::firstConquer(Map &map)
+{
+	std::cout << "Pick a map to conquer" << std::endl;
+	int choice = 0;
+	int i = 1;
+	for (std::shared_ptr<Map::City> region : map.getCities())
+	{
+		std::cout << i << ". " << "City" << std::endl;
+		i++;
+	}
+	std::cin >> choice;
+
+	for (std::shared_ptr<Map::City>& region : map.getCities())
+	{
+		if (choice == region->m_cityNumber && !region->m_isOwned)
+		{
+			region->m_isOwned = true;
+			region->m_tokenSprite = std::make_unique<sf::Sprite>();
+			region->m_tokenSprite->setTexture(ResourceHolder::Instance()->get(Textures::Title));
+			region->m_tokenSprite->setPosition(region->m_xPos, region->m_yPos);
+			m_ownedCities.push_back(region);
+			break;
+		}
+			
+	}
+}
+
+void Player::pickCharacter(std::vector<CharacterToken> m_characterTokens)
+{
+
 }
